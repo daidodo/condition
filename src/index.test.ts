cd4cc867f03c1f088f527ade192b706b9123415a
DoZerg <daidodo@gmail.com>
2022-02-27 09:05:24 +0000

    Implement first isXXX & assertXXX functions.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,26 @@
-import a from './';
+import {
+  assertIsNumber,
+  isNumber,
+} from './';
 
-describe('Suite', () => {
+type A = (number | string)[];
+
+describe('Suite 1', () => {
   it('should pass the test', () => {
-    expect(a).toEqual(1);
+    const a: A = [1, 2, '3', 4];
+    // const r0 = a.filter((n): n is number => typeof n === 'number');
+    const r1 = a.filter(isNumber);
+    expect(r1).toStrictEqual([1, 2, 4]);
+  });
+});
+describe('Suite 2', () => {
+  it('should pass the test', () => {
+    const b: A = [1, 2, 3];
+    const r2 = b.map(n => {
+      assertIsNumber(n);
+      return n;
+    });
+    expect(r2).toStrictEqual([1, 2, 3]);
+    // const r3 = a.filter(isType<number>);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,62 @@
-/**
- * This is an example.
- */
-const a = 1;
+export function assertTrue(
+  condition: boolean,
+  message?: string,
+  props?: object,
+): asserts condition {
+  if (!condition) throwError(message ?? `Expected true but got ${condition}`, props);
+}
 
-export default a;
+export function assertFalse(
+  condition: boolean,
+  message?: string,
+  props?: object,
+): asserts condition is false {
+  if (!!condition) throwError(message ?? `Expected false but got ${condition}`, props);
+}
+
+export function isNonNull<T>(value: T): value is NonNullable<T> {
+  return value !== undefined && value !== null;
+}
+
+export function assertNonNull<T>(
+  value: T,
+  message?: string,
+  props?: object,
+): asserts value is NonNullable<T> {
+  if (!isNonNull(value)) throwError(message ?? `Expected non-null value but got ${value}`, props);
+}
+
+export const isNumber = isType<number>('number');
+export const isString = isType<string>('string');
+
+// https://github.com/microsoft/TypeScript/issues/41047#issuecomment-706752079
+const _assertIsNumber = assertIsType<number>(
+  isNumber,
+  v => `Expected a number but got ${typeof v}: ${v}`,
+);
+const _assertIsString = assertIsType<string>(
+  isNumber,
+  v => `Expected a string but got ${typeof v}: ${v}`,
+);
+
+export const assertIsNumber: typeof _assertIsNumber = _assertIsNumber;
+export const assertIsString: typeof _assertIsString = _assertIsString;
+
+type TypeName = 'number' | 'string' | 'boolean' | 'function' | 'bigint' | 'object';
+
+function isType<T>(name: TypeName) {
+  return (value: any): value is T => {
+    return typeof value === name;
+  };
+}
+
+function assertIsType<T>(check: (v: any) => boolean, defaultMsg: (v: any) => string) {
+  return (value: any, message?: string, props?: object): asserts value is T => {
+    if (!check(value)) throwError(message ?? defaultMsg(value), props);
+  };
+}
+
+function throwError(msg: string, props?: object) {
+  const pmsg = props ? `, props = ${JSON.stringify(props)}` : '';
+  throw Error(msg + pmsg + '.');
+}
